c790e41a9b9d707d2ce643439d5f0b9bb79454cb
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-05 16:15:19 +0800

    initialize Main ui

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,20 @@
+import './styles.css'
+
+function Main() {
+  return (
+    <main>
+      <div id='main-container'>
+        <div>
+          <h1>Little Lemon</h1>
+          <h3>Chicago</h3>
+        </div>
+        <div>
+          <h1>Little Lemon</h1>
+          <h3>Chicago</h3>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Main
