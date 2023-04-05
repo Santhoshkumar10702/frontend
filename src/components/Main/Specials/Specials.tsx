4a266eb8de0f73f8cf6b14e3572f4c5a0ba8d8f2
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-05 18:53:34 +0800

    finish specials section

--- src/components/Main/Specials/Specials.tsx
+++ src/components/Main/Specials/Specials.tsx
@@ -1,5 +1,6 @@
+import { greekSalad } from '../../../icons_assets'
 import './styles.css'
-
+import { FaMotorcycle } from 'react-icons/fa';
 function Specials() {
   return (
     <section id='specials'>
@@ -10,6 +11,41 @@ function Specials() {
         <button className='main-button'>
           Online Menu
         </button>
+      </div>
+      <div className='specials-section'>
+        <div className='special'>
+          <img src={greekSalad} />
+          <div>
+            <div className='title-section'>
+              <h4>Greek salad</h4>
+              <p >$12.99</p>
+            </div>
+            <p className='special-desc'>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+            <button>Order a delivery&nbsp;<FaMotorcycle size={20} /></button>
+          </div>
+        </div>
+        <div className='special'>
+          <img src={greekSalad} />
+          <div>
+            <div className='title-section'>
+              <h4>Greek salad</h4>
+              <p >$12.99</p>
+            </div>
+            <p className='special-desc'>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+            <button>Order a delivery&nbsp;<FaMotorcycle size={20} /></button>
+          </div>
+        </div>
+        <div className='special'>
+          <img src={greekSalad} />
+          <div>
+            <div className='title-section'>
+              <h4>Greek salad</h4>
+              <p >$12.99</p>
+            </div>
+            <p className='special-desc'>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+            <button>Order a delivery&nbsp;<FaMotorcycle size={20} /></button>
+          </div>
+        </div>
       </div>
     </section>
   )
--- src/icons_assets/index.ts
+++ src/icons_assets/index.ts
@@ -1,4 +1,5 @@
 import { ReactComponent as Logo } from './Logo.svg'
 import restaurantFood from './restauranfood.jpg'
+import greekSalad from './greek salad.jpg'
 
-export { Logo, restaurantFood }
+export { Logo, restaurantFood, greekSalad }
