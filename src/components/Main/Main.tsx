142f97298815cfad7fe809249069df0543e9793e
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-05 17:03:01 +0800

    initialize specials

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,23 +1,26 @@
 import { restaurantFood } from '../../icons_assets'
+import Specials from './Specials'
 import './styles.css'
 
 function Main() {
   return (
     <main>
-      <div id='main-container'>
-        <div id='main-left-section'>
-          <h1>Little Lemon</h1>
-          <h3>Chicago</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <button>Reserve a table</button>
-        </div>
-        <div id='main-img'>
-          <img src={restaurantFood} />
-
+      <div id='main-top-section'>
+        <div id='main-container'>
+          <div id='main-left-section'>
+            <h1>Little Lemon</h1>
+            <h3>Chicago</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button>Reserve a table</button>
+          </div>
+          <div id='main-img'>
+            <img src={restaurantFood} />
+          </div>
         </div>
       </div>
+      <Specials />
     </main>
   )
 }
