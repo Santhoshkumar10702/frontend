d1ec51103586838f672ea8a07af4959c67264a2b
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-05 20:36:27 +0800

    add testimonials UI

--- src/components/Main/CustomersSay/CustomersSay.tsx
+++ src/components/Main/CustomersSay/CustomersSay.tsx
@@ -1,5 +1,7 @@
 import {useId} from 'react';
 
+import {user1, user2, user3} from '../../../icons_assets';
+
 import './styles.css';
 
 function CustomersSay() {
@@ -7,11 +9,12 @@ function CustomersSay() {
 
   return (
     <section id="testimonials">
+      <h1>Testimonials</h1>
       <div className="ratings">
         {reviews.map((r, i) => (
           <div key={`${id}-${i}`} className="rating">
             <div className="user-section">
-              <img />
+              <img src={r.image} />
               <h4>{r.name}</h4>
             </div>
             <p>{r.review}</p>
@@ -28,13 +31,16 @@ const reviews = [
   {
     name: 'Jasmine Smith',
     review: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor',
+    image: user1,
   },
   {
     name: 'Phil Jackson',
     review: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor',
+    image: user2,
   },
   {
     name: 'Kobe Bryant',
     review: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor',
+    image: user3,
   },
 ];
--- src/icons_assets/index.ts
+++ src/icons_assets/index.ts
@@ -1,7 +1,19 @@
-import { ReactComponent as Logo } from './Logo.svg';
-import { ReactComponent as Bruchetta } from './bruchetta.svg'
-import restaurantFood from './restauranfood.jpg'
-import greekSalad from './greek salad.jpg'
-import lemonDessert from './lemon dessert.jpg'
+import {ReactComponent as Logo} from './Logo.svg';
+import {ReactComponent as Bruchetta} from './bruchetta.svg';
+import restaurantFood from './restauranfood.jpg';
+import greekSalad from './greek salad.jpg';
+import lemonDessert from './lemon dessert.jpg';
+import user1 from './user_1.png';
+import user2 from './user_2.png';
+import user3 from './user_3.png';
 
-export { Logo, restaurantFood, greekSalad, Bruchetta, lemonDessert }
+export {
+  Logo,
+  restaurantFood,
+  greekSalad,
+  Bruchetta,
+  lemonDessert,
+  user1,
+  user2,
+  user3,
+};
