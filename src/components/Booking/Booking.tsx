0c9f52b5793ffa8eef56fec85be55cf0a6527999
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-06 11:49:01 +0800

    add form

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,5 +1,33 @@
+import './styles.css';
+
 function Booking() {
-  return <div>Booking</div>;
+  return (
+    <section id="booking">
+      <h1>Booking</h1>
+      {/*  <form > */}
+      <form style={{display: 'grid', maxWidth: 200, gap: 20}}>
+        <label htmlFor="res-date">Choose date</label>
+        <input type="date" id="res-date" />
+        <label htmlFor="res-time">Choose time</label>
+        <select id="res-time ">
+          <option>17:00</option>
+          <option>18:00</option>
+          <option>19:00</option>
+          <option>20:00</option>
+          <option>21:00</option>
+          <option>22:00</option>
+        </select>
+        <label htmlFor="guests">Number of guests</label>
+        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        <label htmlFor="occasion">Occasion</label>
+        <select id="occasion">
+          <option>Birthday</option>
+          <option>Anniversary</option>
+        </select>
+        <input type="submit" value="Make Your reservation" />
+      </form>
+    </section>
+  );
 }
 
 export default Booking;
