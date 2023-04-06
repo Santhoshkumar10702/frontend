ebfc750f1ec3732daef1391da29f9e01b3e9ee71
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-06 17:14:13 +0800

    display confirmation info

--- src/components/BookingConfirmation/BookingConfirmation.tsx
+++ src/components/BookingConfirmation/BookingConfirmation.tsx
@@ -2,12 +2,30 @@ import {useConfirmationReducer} from '../../store';
 import './styles.css';
 
 function BookingConfirmation() {
-  const confirmation = useConfirmationReducer();
+  const {state} = useConfirmationReducer();
   return (
     <section id="booking-confirmation">
       <div className="booking-confirmation-section">
-        {confirmation.state.confirmation ? (
-          <h1>Booking Confirmed</h1>
+        {state.confirmation ? (
+          <div>
+            <h1>Booking Confirmed</h1>
+            <div>
+              <h3>Date</h3>
+              <span>{state.confirmation.date}</span>
+            </div>
+            <div>
+              <h3>Time</h3>
+              <span>{state.confirmation.time}</span>
+            </div>
+            <div>
+              <h3>Number of Guests</h3>
+              <span>{state.confirmation.numberOfGuests}</span>
+            </div>
+            <div>
+              <h3>Occasion</h3>
+              <span>{state.confirmation.occasion}</span>
+            </div>
+          </div>
         ) : (
           <h1>No Booking Confirmed at the Moment</h1>
         )}
